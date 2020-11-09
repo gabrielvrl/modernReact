65fa60ba247394c91c8d99c95b688393bcf6ffc4
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2020-11-09 15:27:28 -0300

    handling errors and loading

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,7 +7,7 @@ class App extends React.Component{
 
     // this is the only time we do direct assignment
     // to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
     
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -17,13 +17,23 @@ class App extends React.Component{
         // we should never do:
         // this.state.lat = position.coords.latitude
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
     );
   }
 
   // React says we have to define render
   render(){
-    return <div>Latitude: {this.state.lat}</div>;
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat){
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
