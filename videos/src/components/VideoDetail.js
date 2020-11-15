5e25b6d56be368fa5a163644fd0e23de1083b023
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2020-11-15 12:17:13 -0300

    showing video

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const VideoDetail = ({video}) => {
-  if(!video){
-    return <div>Loading...</div>
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
   }
 
-  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return (
     <div>
@@ -18,6 +18,6 @@ const VideoDetail = ({video}) => {
       </div>
     </div>
   );
-}
+};
 
 export default VideoDetail;
