74baacfbcfda9459fcefda01bd957b9461a0b62b
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2020-11-18 07:25:54 -0300

    dropdown app

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -18,10 +18,31 @@ const items = [
   }
 ];
 
+const options = [
+  {
+    label:'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of Blue',
+    value:'blue',
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return(
     <div>
-      <Dropdown />
+      <Dropdown 
+        selected={selected} 
+        onSelectedChange={setSelected}
+        options={options} 
+      />
     </div>
   )
 };
