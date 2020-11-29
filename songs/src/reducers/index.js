f8d47d1672a87d98fe7d0ca34af798d9ff88e989
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2020-11-29 20:27:20 -0300

    selectedSongReducer

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+const { selectSong } = require("../actions");
+
 const songsReducer = () => {
     return [
         { title: 'No Scrubs', duration: '4:05' },
@@ -5,4 +7,12 @@ const songsReducer = () => {
         { title: 'All Star', duration: '3:15' },
         { title: 'I Want it That Way', duration: '1:45' }
     ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
 };
