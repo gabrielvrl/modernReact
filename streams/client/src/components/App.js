66cb10b2e587e395a6f55f8d1bb098ff207f3340
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-08 19:50:01 -0300

    Adding Header

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './Header'
+
 import StreamCreate from './streams/StreamCreate'
 import StreamEdit from './streams/StreamEdit'
 import StreamDelete from './streams/StreamDelete'
@@ -9,8 +11,9 @@ import StreamShow from './streams/StreamShow'
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
+                <Header />
                 <Route path="/" exact component={StreamList} />
                 <Route path="/streams/new" exact component={StreamCreate} />
                 <Route path="/streams/edit" exact component={StreamEdit} />
