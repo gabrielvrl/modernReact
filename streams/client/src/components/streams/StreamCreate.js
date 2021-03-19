fe93f66d0ab5137b9242fc33429d6a894dfcd7eb
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-19 16:34:03 -0300

    creating first stream

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -27,8 +27,8 @@ class StreamCreate extends React.Component {
         )
     }
 
-    onSubmit(formValues){
-        console.log(formValues)
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues)
     }
 
 
@@ -57,7 +57,9 @@ const validate = (formValues) => {
     return errors
 }
 
-export default reduxForm({ 
+const formWrapped = reduxForm({ 
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped)
