6e45cde703dd4d63a6586cf4d58f77f34b7ad60b
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-04-16 11:07:21 -0300

    creating history

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import history from "../history";
 
-import Header from './Header'
+import Header from "./Header";
 
-import StreamCreate from './streams/StreamCreate'
-import StreamEdit from './streams/StreamEdit'
-import StreamDelete from './streams/StreamDelete'
-import StreamList from './streams/StreamList'
-import StreamShow from './streams/StreamShow'
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <Header />
                 <Route path="/" exact component={StreamList} />
                 <Route path="/streams/new" exact component={StreamCreate} />
                 <Route path="/streams/edit" exact component={StreamEdit} />
                 <Route path="/streams/delete" exact component={StreamDelete} />
                 <Route path="/streams/show" exact component={StreamShow} />
-            </BrowserRouter>
+            </Router>
         </div>
-    )
+    );
 };
 
 export default App;
