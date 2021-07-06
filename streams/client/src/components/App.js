ccd442e2c1b4995938092346fdfa4fc547b92036
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-07-06 20:55:47 -0300

    Switches with React-Router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import history from "../history";
 
 import Header from "./Header";
@@ -11,18 +11,20 @@ import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 
 const App = () => {
-    return (
-        <div className="ui container">
-            <Router history={history}>
-                <Header />
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                <Route path="/streams/:id" exact component={StreamShow} />
-            </Router>
-        </div>
-    );
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          <Route path="/streams/:id" exact component={StreamShow} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
