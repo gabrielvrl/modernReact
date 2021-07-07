f090167de2fae2910375cfbc92677d9326a61430
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-07-06 21:38:45 -0300

    Player Building

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -12,9 +12,21 @@ class StreamShow extends React.Component {
 
   componentDidMount() {
     const { id } = this.props.match.params;
+    this.props.fetchStream(id);
 
-    this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
 
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: "flv",
       url: `http://localhost:8000/live/${id}.flv`,
@@ -23,6 +35,7 @@ class StreamShow extends React.Component {
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
   }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
